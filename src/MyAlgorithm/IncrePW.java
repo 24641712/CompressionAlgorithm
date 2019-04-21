315d948faeab80b276e7866bd1702edc969875b4
24641712 <2464171268@qq.com>
2019-04-21 15:13:03 +0800

    新算法实现完善细节

--- src/MyAlgorithm/IncrePW.java
+++ src/MyAlgorithm/IncrePW.java
@@ -9,25 +9,53 @@ import utils.GetTime;
 import java.util.ArrayList;
 
 /**
+ * 基本思想：假定每一个轨迹点是特征点，求该特征点到两边点的
+ * 垂直距离，若距离大于阈值则判定该点是特征点，否则不是特征
+ * 点。
  * @Author ccl
  * @Date 2019/4/20
  */
 public class IncrePW {
     static double LimitDis;
 
-    public static double getMaxDist(ArrayList<Point> beforeTraj,int index){
+    public static double getMaxDist(ArrayList<Point> beforeTraj,int index,int tzd){
         Distance distance = new Distance();
         double maxdis=0,curdis;
-        int left,right;
+        int left = index - 1;
+        int right = index + 1;
         boolean leftFlag,rightFlag;
-        leftFlag = rightFlag = false;
-        while(){
-
-
-
+        int lenght = beforeTraj.size();
+        leftFlag = rightFlag = true;
+        Point pc = beforeTraj.get(index);
+        while(leftFlag||rightFlag){
+            if(left>=0&&leftFlag){
+                Point pa = beforeTraj.get(left);
+                Point pb = beforeTraj.get(right);
+                curdis = distance.getDistance(pa,pb,pc);
+                if(maxdis<curdis){
+                   maxdis = curdis;
+                }else {
+                    left++;
+                    leftFlag = false;
+                }
+            }
+            if(right<lenght){
+                Point pa = beforeTraj.get(left);
+                Point pb = beforeTraj.get(right+1);
+                curdis = distance.getDistance(pa,pb,pc);
+                if(maxdis<curdis){
+                    maxdis = curdis;
+                }else{
+                    right--;
+                    rightFlag = false;
+                }
+            }
+            if(left<=0)leftFlag = false;
+            //left不能越过上一个特征点
+            left = left>tzd?left--:tzd;
+            if(rightFlag)
+            right = right<lenght?++right:(lenght-1);
         }
-
-
         return maxdis;
     }
 
@@ -38,14 +66,19 @@ public class IncrePW {
        boolean leftFlag = true;
        boolean rightFlag = true;
        double result = 0;
-       double maxdis;
+       int tzd = 0;//特征点
+       double maxdis = 0,curdis;
        for(int i=1;i<length;i++){
-
-
-
-
-
-
+           curdis = getMaxDist(beforeTraj,i,tzd);
+           //不是特征点
+           if(maxdis<curdis){
+               maxdis = curdis;
+           }else{
+               if(maxdis>LimitDis){
+                   afterTraj.add(beforeTraj.get(i));
+               }
+               maxdis = 0;
+           }
        }
        return afterTraj;
     }
